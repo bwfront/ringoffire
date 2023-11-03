79bf22b1e498a7562aea0aedf9eb99e3705d245c
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-03 21:48:52 +0100

    Implement Card Animation and Include Cards with Function

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  takeCard(){
-    this.pickCardAnimation = true;
+  lastCard = false;
+  currentCard: string = '';
+  playedCard: string = '';
+  game: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation && this.game.stack.length > 0) {
+      this.currentCard = this.game.stack.pop();
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.lastCard = true;
+        this.playedCard = this.currentCard;
+        this.pickCardAnimation = false;
+      }, 1500);
+    }
+  }
+
+  newGame() {
+    this.game = new Game();
   }
 }
