9a9e337db06434b955148733d3cb2a5f2d922087
Bennet Witczak <bennet.witczak@icloud.com>
2023-11-03 19:13:23 +0100

    Implement Start Game Button // Game Component - Render Card field - show cards

--- src/app/startscreen/startscreen.component.ts
+++ src/app/startscreen/startscreen.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-startscreen',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class StartscreenComponent {
 
+  constructor(private routuer: Router){}
+
+  newGame(){
+    this.routuer.navigateByUrl('/game');
+  }
 }
